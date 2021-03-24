cecaf932fda23b311976a0ed0a17adc1d8496a33
KayhanKishi <72473860+KayhanKishi@users.noreply.github.com>
2021-03-24 14:46:33 -0400

    Login Files
    bottom.html and top.php can be included on the tops and bottom of many pages. There's no styling on the pages. Any and all styling I did was ugly and didn't fit with everyone else's work so I removed it.

---
+++ hangman/src/top.php
@@ -0,0 +1,28 @@
+<html>
+	
+	<head>
+		<title>Hangman</title>
+		
+		<meta charset="utf-8" />
+		
+		<link href="noose.png" type="image/png" rel="shortcut icon" />
+	<!--	<link href="login.css" type="text/css" rel="stylesheet" /> -->
+		
+	</head>
+
+	<body>
+		<div id="bannerarea">
+		<a href="title.php">
+			<img src="Title.JPG" alt="banner logo" /> <br />
+			</a>
+		</div>
+		<?php
+if(!isset($_COOKIE["name"])) {
+  echo "Username is not set!";
+} else {
+  echo "Logged in as '" .$_COOKIE["name"]. "'<br>";
+  echo "Difficulty is: " . $_COOKIE["difficulty"];
+}
+?>
+</body>
+</html>
