1d6aa64b3dee6dd4b9208b3712181fabf6cc3b89
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-03-24 12:46:37 -0400

    Add files via upload
    play buttons page

---
+++ hangman/src/play.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman Game</title>
+    <link rel="stylesheet" href="format.css" />
+</head>
+<body class = "background">
+	<div class="playlevels">
+		<p>Choose your level...</p>
+	</div>
+	<div class="gameplay-sel">
+        <button class="button level btn"> Easy Level</button>
+        <button class="button level btn"> Intermediate Level</button>
+        <button class="button level btn"> Hard Level</button>
+        <button class="button level btn"> Master Level</button>
+    </div>
+</body>
+</html>
