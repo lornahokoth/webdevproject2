3d42e1f5e8cb7a084a41b6e1dd894888c9b4b57d
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-03-24 13:56:57 -0400

    Update title.php
    buttons for play, rules, leaderboard added

---
+++ hangman/src/title.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman Game</title>
+    <link rel="stylesheet" href="format.css" />
+</head>
+<body class="background">
+	<div class="welcome">
+		<h1>Welcome, *username*</h1>
+	</div>
+	<div class="tdisplay">
+		<a href=""><button class="button level btn"> PLAY </button></a>
+		<br>
+        <button class="button level btn"> RULES </button>
+		<br>
+        <button class="button level btn"> LEADERBOARD </button>
+		
+	</div>
+</body>
+</html>
