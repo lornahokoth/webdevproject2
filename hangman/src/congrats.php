e9a341e6c151e97e9e10833e8bbfff0feb77effc
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-03-24 23:15:39 -0400

    Add files via upload
    congrats.php // trying to test time

---
+++ hangman/src/congrats.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman Game</title>
+    <link rel="stylesheet" href="../css/format.css" />
+    <link rel="icon" href="../assets/Logo.png" type="image/x-icon" />
+</head>
+
+<body class="background">
+<?php
+	echo "<p>This is your time: ".$total_time."<p>";
+?>
+</body>
+</html>
