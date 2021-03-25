4d5579111bdf333b25f39e1de9f29ab5029ec6a0
Abu Nadim <nadimkabir2@gmail.com>
2021-03-24 23:09:37 -0400

    updated verify update method from leaderboard.php

--- hangman/src/leaderboard.php
+++ hangman/src/leaderboard.php
@@ -1,8 +1,8 @@
 <?php
 
-$Name = "john";
+$Name = "bob";
 $Difficulty = "I";
-$Score = "10001";
+$Score = "10010";
 setcookie("name", $Name, time() + 100, "/");
 setcookie("difficulty", $Difficulty, time() + 100, "/");
 setcookie("score", "$Score", time() + 100, "/");
@@ -35,76 +35,163 @@ setcookie("score", "$Score", time() + 100, "/");
 // { }
 
 
-function($cookie_name,$cookie_difficulty,$cookie_score, $filepath){
+function update_leaderboard($cookie_name, $cookie_difficulty, $cookie_score, $filepath)
+{
+    if (isset($cookie_name) and isset($cookie_difficulty) and isset($cookie_score)) {
 
-    
-}
-if (isset($_COOKIE["name"]) and isset($_COOKIE["difficulty"]) and isset($_COOKIE["score"])) {
+        // print_r("inside isset");
 
+        // print_r("cookie_username: " . $cookie_name . " cookie_difficulty: " . $cookie_difficulty . " cookie_score: " . $cookie_score);
 
-    print_r("inside isset");
-    $cookie_name = $_COOKIE["name"];
-    $cookie_difficulty = $_COOKIE["difficulty"];
-    $cookie_score = $_COOKIE["score"];
+        $fh = fopen($filepath, 'r+');
+        $file = file($filepath);
 
-    print_r("cookie_username: " . $cookie_name . " cookie_difficulty: " . $cookie_difficulty . " cookie_score: " . $cookie_score);
-    $fh = fopen("../textfile/leaderboard.txt", 'r+');
-    $file = file("../textfile/leaderboard.txt");
+        $pattern = "/\b($cookie_name)\|($cookie_difficulty)\|\b/i";
+        $matches = preg_grep($pattern, $file);
+        $line_num = array_keys(preg_grep($pattern, $file));
+        $line_num = $line_num[0];
 
-    $pattern = "/\b($cookie_name)\|($cookie_difficulty)\|\b/i";
-    $matches = preg_grep($pattern, $file);
-    $line_num = array_keys(preg_grep($pattern, $file));
-    $line_num = $line_num[0];
+        // string to put username and passwords
+        $users = '';
 
-    // string to put username and passwords
-    $users = '';
+        if (count($matches) > 0) {
+            // print_r("more that one match found <br>");
+            while (!feof($fh)) {
+                $user = explode('|', fgets($fh));
 
-    var_dump($matches);
-    var_dump($line_num);
+                // take-off old "\r\n"
+                $username = trim($user[0]);
+                $difficulty = trim($user[1]);
+                $score = trim($user[2]);
 
-    if (count($matches) > 0) {
-        $i = 0;
-        print_r("more that one match found");
-        while (!feof($fh)) {
-            if ($line_num == $i) {
-                print("|| true ||");
-            }
-            $i++;
-            $user = explode('|', fgets($fh));
-            print_r($user);
+                // print_r("username: " . $username . " difficulty: " . $difficulty . " score: " . $score);
 
-            // take-off old "\r\n"
-            $username = trim($user[0]);
-            $difficulty = trim($user[1]);
-            $score = trim($user[2]);
+                // check for empty indexes
+                // var_dump(!empty($username) and !empty($difficulty) and !empty($score));
+                if (!empty($username) and !empty($difficulty) and !empty($score)) {
+                    // print_r("<br>| not empty");
+                    if (($username == $cookie_name) and ($difficulty == $cookie_difficulty)) {
+                        // print_r("| conditions met");
+                        if ((intval($cookie_score) > intval($score))) {
+                            $score = $cookie_score;
+                        } else {
+                            return false;
+                        }
+                    }
 
-            print_r("username: " . $username . " difficulty: " . $difficulty . " score: " . $score);
-
-            // check for empty indexes
-            // var_dump(!empty($username) and !empty($difficulty) and !empty($score));
-            if (!empty($username) and !empty($difficulty) and !empty($score)) {
-                print_r("<br>| not empty");
-                if (($username == $cookie_name) and ($difficulty == $cookie_difficulty) and (intval($cookie_score) > intval($score))) {
-                    print_r("| conditions met");
-                    $score = $cookie_score;
+                    $users .= $username . '|' . $difficulty . '|' . $score . "\n";
+                    // print_r("users: ", $users);
+                    // $users .= "\r\n";
                 }
-
-                $users .= $username . '|' . $difficulty . '|' . $score . "\n";
-                print_r("users: ", $users);
-                // $users .= "\r\n";
             }
-        }
 
-        // using file_put_contents() instead of fwrite()
-        file_put_contents('../textfile/leaderboard.txt', $users);
-    } else if (count($matches) == 0) {
-        print_r("no matches found");
-        $text = $cookie_name . '|' . $cookie_difficulty . '|' . $cookie_score;
-        file_put_contents('../textfile/leaderboard.txt', $text, FILE_APPEND);
+            // using file_put_contents() instead of fwrite()
+            file_put_contents('../textfile/leaderboard.txt', $users);
+        } else if (count($matches) == 0) {
+            // print_r("no matches found");
+            $text = "\n" . $cookie_name . '|' . $cookie_difficulty . '|' . $cookie_score;
+            file_put_contents('../textfile/leaderboard.txt', $text, FILE_APPEND);
+        }
+        fclose($fh);
+        fclose($file);
+        return;
     }
-    fclose($fh);
-    fclose($file);
 }
+
+function verify_update($cookie_name, $cookie_difficulty, $cookie_score, $filepath)
+{
+    $file = file($filepath);
+    $pattern = "/\b($cookie_name)\|($cookie_difficulty)\|($cookie_score)\b/i";
+    $matches = preg_grep($pattern, $file);
+
+    if (count($matches) == 0) {
+        print_r("<br><br>match not found<br><br>");
+        return false;
+    } else {
+        print_r("match found");
+        return true;
+    }
+}
+
+print_r("Cookie Value: <br><br>" . $_COOKIE['name'] . " | " . $_COOKIE["difficulty"] . " | " . $_COOKIE['score']);
+
+if (verify_update($_COOKIE['name'], $_COOKIE["difficulty"], $_COOKIE['score'], '../textfile/leaderboard.txt')) {
+    print_r("<br><br>page updated");
+} else if (!update_leaderboard($_COOKIE['name'], $_COOKIE["difficulty"], $_COOKIE['score'], '../textfile/leaderboard.txt')) {
+    print_r("cookie value of " . $_COOKIE["score"] . " is less than the original value");
+} else if (!verify_update($_COOKIE['name'], $_COOKIE["difficulty"], $_COOKIE['score'], '../textfile/leaderboard.txt')) {
+    print("<br><br>updating...<br><br>");
+    update_leaderboard($_COOKIE['name'], $_COOKIE["difficulty"], $_COOKIE['score'], '../textfile/leaderboard.txt');
+}
+
+
+
+// if (isset($_COOKIE["name"]) and isset($_COOKIE["difficulty"]) and isset($_COOKIE["score"])) {
+
+
+//     print_r("inside isset");
+//     $cookie_name = $_COOKIE["name"];
+//     $cookie_difficulty = $_COOKIE["difficulty"];
+//     $cookie_score = $_COOKIE["score"];
+
+//     print_r("cookie_username: " . $cookie_name . " cookie_difficulty: " . $cookie_difficulty . " cookie_score: " . $cookie_score);
+//     $fh = fopen("../textfile/leaderboard.txt", 'r+');
+//     $file = file("../textfile/leaderboard.txt");
+
+//     $pattern = "/\b($cookie_name)\|($cookie_difficulty)\|\b/i";
+//     $matches = preg_grep($pattern, $file);
+//     $line_num = array_keys(preg_grep($pattern, $file));
+//     $line_num = $line_num[0];
+
+//     // string to put username and passwords
+//     $users = '';
+
+//     var_dump($matches);
+//     var_dump($line_num);
+
+//     if (count($matches) > 0) {
+//         $i = 0;
+//         print_r("more that one match found");
+//         while (!feof($fh)) {
+//             if ($line_num == $i) {
+//                 print("|| true ||");
+//             }
+//             $i++;
+//             $user = explode('|', fgets($fh));
+//             print_r($user);
+
+//             // take-off old "\r\n"
+//             $username = trim($user[0]);
+//             $difficulty = trim($user[1]);
+//             $score = trim($user[2]);
+
+//             print_r("username: " . $username . " difficulty: " . $difficulty . " score: " . $score);
+
+//             // check for empty indexes
+//             // var_dump(!empty($username) and !empty($difficulty) and !empty($score));
+//             if (!empty($username) and !empty($difficulty) and !empty($score)) {
+//                 print_r("<br>| not empty");
+//                 if (($username == $cookie_name) and ($difficulty == $cookie_difficulty) and (intval($cookie_score) > intval($score))) {
+//                     print_r("| conditions met");
+//                     $score = $cookie_score;
+//                 }
+
+//                 $users .= $username . '|' . $difficulty . '|' . $score . "\n";
+//                 print_r("users: ", $users);
+//                 // $users .= "\r\n";
+//             }
+//         }
+
+//         // using file_put_contents() instead of fwrite()
+//         file_put_contents('../textfile/leaderboard.txt', $users);
+//     } else if (count($matches) == 0) {
+//         print_r("no matches found");
+//         $text = $cookie_name . '|' . $cookie_difficulty . '|' . $cookie_score;
+//         file_put_contents('../textfile/leaderboard.txt', $text, FILE_APPEND);
+//     }
+//     fclose($fh);
+//     fclose($file);
+// }
 // print_r("inside isset");
 // $cookie_name = $_COOKIE["name"];
 // $cookie_score = $_COOKIE["score"];
